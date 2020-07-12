31c75227b8ea3fb564a38ceb20a24660e1646635
marvinhm <marvinmatovu@gmail.com>
2020-07-12 14:06:24 +0100

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Marvin Matovu on 12/07/2020.
+//  Copyright © 2020 Marvin Matovu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
